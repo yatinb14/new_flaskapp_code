6f39052cf7a4ee5e5682d3d3353c74705ac9d93b
yatinb14 <65548009+yatinb14@users.noreply.github.com>
2021-04-28 17:32:00 +0530

    Update oldamsstart.sh

--- oldamsstart.sh
+++ oldamsstart.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-cd /home/thinknyx/AMS/ams/
+cd /thinknyx/ams/
 source ./newenv
 export FLASK_DEBUG=1
 export FLASK_ENV=development
-cd app
+cd new_code
 export FLASK_APP=login.py
 nohup flask run --host 0.0.0.0 --port 5000 &
 echo "Login started"
-cd ../app
+cd ../new_code
 export FLASK_APP=admin.py
 nohup flask run --host 0.0.0.0 --port 5001 &
 echo "Admin started"
-cd ../app
+cd ../new_code
 export FLASK_APP=home.py
 nohup flask run --host 0.0.0.0 --port 5002 &
 echo "Home started"
-cd ../app
+cd ../new_code
 export FLASK_APP=vendor.py
 nohup flask run --host 0.0.0.0 --port 5003 &
 echo "vendor started"
-cd ../app
+cd ../new_code
 export FLASK_APP=invoice.py
 nohup flask run --host 0.0.0.0 --port 5004 &
 echo "Invoice started"
-cd ../app
+cd ../new_code
 export FLASK_APP=purchase.py
 nohup flask run --host 0.0.0.0 --port 5005 &
 echo "Purchase started"
-cd ../app
+cd ../new_code
 export FLASK_APP=trainer.py
 nohup flask run --host 0.0.0.0 --port 5006 &
 echo "trainer started"
-cd ../app
+cd ../new_code
 export FLASK_APP=report.py
 nohup flask run --host 0.0.0.0 --port 5007 &
 echo "report started"
-cd ../app
+cd ../new_code
 export FLASK_APP=bill.py
 nohup flask run --host 0.0.0.0 --port 5008 &
 echo "bill started"
